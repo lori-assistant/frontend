9fd0558ac33a41998e3453cdff750dd496b85f83
Moritz Riede <moritz@mriede.com>
2023-09-02 00:55:10 +0200

    feat: fix layout and integrate dark theme

--- lib/chat/message.dart
+++ lib/chat/message.dart
@@ -17,21 +17,48 @@ class Message extends StatelessWidget {
       children: [
         Expanded(
           child: Container(
-            color: author == MessageAuthor.ai ? Color(0x11000000) : Colors.transparent,
+            color: MediaQuery.of(context).platformBrightness == Brightness.dark
+              ? author == MessageAuthor.ai
+                ? Color(0xFF021A23)
+                : Colors.transparent
+              : author == MessageAuthor.ai
+                ? Color(0xFFBDDCE7)
+                : Colors.transparent,
             padding: EdgeInsets.symmetric(horizontal: 0.0, vertical: 32.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,  // AI Icon is left-aligned
               children: [
                 Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    CircleAvatar(
-                      backgroundColor: Color(0x44000000),
-                      child: Icon(
-                        author == MessageAuthor.ai
-                          ? Icons.android 
-                          : Icons.person,
-                        color: Colors.white,
+                    Container(
+                      width: MediaQuery.of(context).size.width > 768 ? 768 : double.infinity,
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 40,
+                            height: 40,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(9),
+                              color: author == MessageAuthor.ai
+                                ? Color(0xFF379A5D)
+                                : Color(0xFF37839A)
+                            ),
+                            child: Icon(
+                              author == MessageAuthor.ai
+                                ? Icons.android
+                                : Icons.person,
+                              color: Color(0x44000000),
+                            ),
+                          ),
+
+                          SizedBox(width: 12.0),
+
+                          Text(
+                            author == MessageAuthor.ai ? "Lori" : "User",
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                        ],
                       ),
                     ),
                     SizedBox(height: 12.0),
