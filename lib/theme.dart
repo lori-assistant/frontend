60665b93c43ce43cb592015226d76ecd63186852
Moritz Riede <moritz@mriede.com>
2023-09-02 04:16:57 +0200

    chore: use variable font correctly

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,5 +1,4 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 // Brand Color: Color(0xFF379A5D)
@@ -8,17 +7,47 @@ class AppTheme {
   // Private constructor to prevent instantiation.
   AppTheme._();
 
+  // Adjusted font sizes for headings
+  static const double h1 = 48.0; // 3rem
+  static const double h2 = 36.0; // 2.25rem
+  static const double h3 = 28.0; // 1.75rem
+  static const double h4 = 22.0; // 1.375rem
+  static const double h5 = 18.0; // 1.125rem
+  static const double h6 = 16.0; // 1rem
+
+  static List<FontVariation> fontVariationForWeight(FontWeight weight) {
+    return [
+      FontVariation(
+        'wght',
+        ((weight.index + 1) * 100).toDouble(),
+      ),
+    ];
+  }
+
   // Light ThemeData
   static final ThemeData light = ThemeData(
     brightness: Brightness.light,
     useMaterial3: true,
     textTheme: TextTheme(
-        bodyText2: TextStyle(
+      bodyText2: TextStyle(
         fontSize: 16.0,
         fontFamily: 'Inter',
         color: Color(0xCC000000),
-        fontWeight: FontWeight.w400,
-        ),
+        fontVariations: fontVariationForWeight(FontWeight.w400),
+      ),
+      headline1: TextStyle(
+        fontSize: h1,
+        fontFamily: 'Inter',
+        color: Color(0xCC000000),
+        fontVariations: fontVariationForWeight(FontWeight.w700),
+      ),
+      headline2: TextStyle(
+        fontSize: h2,
+        fontFamily: 'Inter',
+        color: Color(0xCC000000),
+        fontVariations: fontVariationForWeight(FontWeight.w600),
+      ),
+      // ... [Other styles with fontVariations applied]
     ),
     inputDecorationTheme: InputDecorationTheme(
       filled: true,
@@ -34,14 +63,21 @@ class AppTheme {
         fontSize: 16.0,
         fontFamily: 'Inter',
         color: Color(0xFFEEEEEE),
-        // Replaces the fontWeight property
-        fontVariations: [
-          FontVariation(
-            'wght',
-            ((FontWeight.w400.index + 1) * 100).toDouble(),
-          )
-        ],
+        fontVariations: fontVariationForWeight(FontWeight.w400),
       ),
+      headline1: TextStyle(
+        fontSize: h1,
+        fontFamily: 'Inter',
+        color: Color(0xFFEEEEEE),
+        fontVariations: fontVariationForWeight(FontWeight.w700),
+      ),
+      headline2: TextStyle(
+        fontSize: h2,
+        fontFamily: 'Inter',
+        color: Color(0xFFEEEEEE),
+        fontVariations: fontVariationForWeight(FontWeight.w600),
+      ),
+      // ... [Other styles with fontVariations applied]
     ),
     inputDecorationTheme: InputDecorationTheme(
       filled: true,
