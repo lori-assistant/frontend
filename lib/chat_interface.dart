2c9948b07d4fd1517d5a0d5e39fddb72368d438b
Moritz Riede <moritz@mriede.com>
2023-09-02 04:15:54 +0200

    feat: switch to `lori-v4`, use spinkit and fix Markdown demo

--- lib/chat_interface.dart
+++ lib/chat_interface.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 import './chat/message.dart';
+import './markdown.dart';
 
 class ChatInterface extends StatefulWidget {
   @override
@@ -15,7 +17,7 @@ class _ChatInterfaceState extends State<ChatInterface> {
   String? _errorMessage;
   String _aiPartialMessage = "Processing...";
   bool _firstTokenReceived = false;  // Track if the first token has been received
-
+/*
   @override
   void initState()
   {
@@ -23,20 +25,40 @@ class _ChatInterfaceState extends State<ChatInterface> {
 
     // Add a user mock message
     _messageWidgets.add(
-      _buildUserMessageWidget("Hello, can you provide me with some information?")
+      _buildUserMessageWidget("Provide a Python implementation of the Fibonacci function?")
     );
 
     // Add an AI mock response with Lorem Ipsum text
     _messageWidgets.add(
       _buildAiMessageWidget(
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
-        "Sed sit amet facilisis urna. Praesent ac gravida libero. "
-        "Donec non tortor sit amet. Vivamus mattis pharetra tellus sed placerat. "
-        "Duis magna purus, elementum eu placerat eu, egestas vitae felis."
+        """
+# Fibonacci Sequence
+
+The Fibonacci sequence is a series of numbers where a number is the sum of the two preceding ones. It usually starts with 0 and 1. In mathematical terms, the sequence `F(n)` is defined by the recurrence relation:
+
+Below is a simple Python function that calculates the Fibonacci number for a given `n`:
+
+```python
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+# Test the function
+for i in range(10):
+    print(fibonacci(i))
+```
+
+This function uses a recursive approach to compute the Fibonacci number. When called with `n`, it returns the `n-th` number in the Fibonacci sequence. The function works by breaking down the problem into two smaller sub-problems: `fibonacci(n-1)` and `fibonacci(n-2)`. These sub-problems are solved recursively until reaching the base cases of `n=0` or `n=1`.
+
+However, it's worth noting that this recursive approach can be inefficient for large values of `n` due to the repeated computation of the same Fibonacci numbers multiple times. There are more efficient algorithms, like the iterative approach or using memoization, to compute Fibonacci numbers in a more optimized manner.
+
+        """
       )
     );
   }
-
+*/
 
   Future<void> sendRequest() async {
     String userMessage = _inputTextController.text;
@@ -50,7 +72,7 @@ class _ChatInterfaceState extends State<ChatInterface> {
       Uri.parse('http://localhost:11434/api/generate'),
     )
       ..headers['Content-Type'] = 'application/json'
-      ..body = '{"model":"lori-v3", "prompt": "$userMessage"}';
+      ..body = '{"model":"lori-v4", "prompt": "$userMessage"}';
 
     try {
       final response = await request.send();
@@ -92,10 +114,17 @@ class _ChatInterfaceState extends State<ChatInterface> {
     );
   }
   Widget _buildAiMessageWidget(String message) {
-    return Message(
-      author: MessageAuthor.ai,
-      text: message,
-    );
+    if (message == "Processing...") {
+      return SpinKitDoubleBounce(
+        color: Color(0xFF358097), // You can customize the color
+        size: 20.0,        // You can customize the size
+      );
+    } else {
+      return Message(
+        author: MessageAuthor.ai,
+        text: message,
+      );
+    }
   }
 
   // Placeholder for no messages
