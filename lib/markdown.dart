f3856058f97fbc9596681ae100137a6ae8c99c1e
Moritz Riede <moritz@mriede.com>
2023-09-03 03:56:24 +0200

    refactor: simplify MarkdownRenderer by extrapolating widgets into semantic components

--- lib/markdown.dart
+++ lib/markdown.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:markdown/markdown.dart' as md;
-import 'package:flutter_highlight/flutter_highlight.dart';
-import 'package:flutter_highlight/themes/solarized-dark.dart';
 
-final customTheme = Map<String, TextStyle>.from(solarizedDarkTheme)
-  ..update('root', (value) => value.copyWith(backgroundColor: Colors.transparent));
+import './design_system/design_system.dart';
 
 class MarkdownRenderer extends StatelessWidget {
   final String source;
@@ -24,97 +21,49 @@ class MarkdownRenderer extends StatelessWidget {
           print("tag: ${node.tag}");
           switch (node.tag) {
             case 'h1':
-              return Container(
-                child: SelectableText(node.textContent, style: Theme.of(context).textTheme.headline1),
-                margin: EdgeInsets.only(top: 24.0),
-              );
+              return LHeading(text: node.textContent, level: 1);
+
             case 'h2':
-              return Container(
-                child: SelectableText(node.textContent, style: Theme.of(context).textTheme.headline2),
-                margin: EdgeInsets.only(top: 24.0),
-              );
+              return LHeading(text: node.textContent, level: 2);
+
+            case 'h3':
+              return LHeading(text: node.textContent, level: 3);
+
+            case 'h4':
+              return LHeading(text: node.textContent, level: 4);
+
+            case 'h5':
+              return LHeading(text: node.textContent, level: 5);
+
+            case 'h6':
+              return LHeading(text: node.textContent, level: 6);
+
             case 'p':
-              return Container(
-                margin: EdgeInsets.only(top: 24.0),
-                child: SelectableText(
-                  node.textContent,
-                  style: Theme.of(context).textTheme.bodyText1
-                ),
-              );
+              return LParagraph(text: node.textContent);
+
             case 'pre':
-              // Assuming the code is wrapped in a <code> tag inside the <pre> tag
               final codeNode = node.children?.first as md.Element;
               final codeContent = codeNode.textContent ?? '';
-              // Extract the language string from the <code> tag's attributes
               final language = codeNode.attributes['class']?.split('language-')?.last;
-              return Container(
-                padding: EdgeInsets.symmetric(horizontal: 18.0, vertical: 9.0),
-                margin: EdgeInsets.symmetric(horizontal: 0.0, vertical: 24.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(9.0),
-                  color: MediaQuery.of(context).platformBrightness == Brightness.dark
-                    ? Color(0xFF000E13)
-                    : Colors.white,
-                ),
-                child: HighlightView(
-                  // Remove all whitespace on the right side.
-                  codeContent.trimRight(),
-                  language: language ?? 'plaintext', // Use 'plaintext' as a fallback if no language is specified
-                  theme: customTheme,
-                  textStyle: TextStyle(fontFamily: 'Inconsolata', fontSize: 18),
-                ),
-              );
+              return LCodeBlock(content: codeContent, language: language);
+
             case 'ul':
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children:
-                    node.children?.map((child) => _buildListItem(context, child))?.toList() ?? [],
-              );
+              final items = node.children?.map((child) => child.textContent)?.toList() ?? [];
+              return LUnorderedList(items: items);
+
             case 'ol':
-              var index = 1;
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children:
-                    node.children?.map((child) => _buildOrderedList(context, child, index++))?.toList() ?? [],
-              );
-            // Add more cases as needed for other element types
+              final items = node.children?.map((child) => child.textContent)?.toList() ?? [];
+              return LOrderedList(items: items);
+
+            case 'blockquote':
+              return LBlockquote(text: node.textContent);
+
+            case 'hr':
+              return LDivider();
           }
         }
-
         return SizedBox.shrink();  // Fallback for unrecognized node types
       }).toList(),
     );
-  }
-
-  Widget _buildListItem(BuildContext context, md.Node node) {
-    if (node is md.Element && node.tag == 'li') {
-      return Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text('• ', style: Theme.of(context).textTheme.bodyText1),
-          Expanded(
-            child:
-                SelectableText(node.textContent, style: Theme.of(context).textTheme.bodyText1),
-          ),
-        ],
-      );
-    }
-    return SizedBox.shrink();
-  }
-
-  Widget _buildOrderedList(BuildContext context, md.Node node, int index) {
-    if (node is md.Element && node.tag == 'li') {
-      return Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text('$index. ', style: Theme.of(context).textTheme.bodyText1),
-          Expanded(
-            child:
-                SelectableText(node.textContent, style: Theme.of(context).textTheme.bodyText1),
-          ),
-        ],
-      );
-    }
-    return SizedBox.shrink();
   }
 }
